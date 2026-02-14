fdd519e2431eaf2e0fd2c20f302534dff833e3ba
rialhasan <realhasan74@gmail.com>
2026-02-14 11:59:52 +0530

    first commit

---
+++ src/test/java/acetcse/csecodecoverage/CseaddTest.java
@@ -0,0 +1,21 @@
+package acetcse.csecodecoverage;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CseaddTest {
+	Cseadd c=new Cseadd();
+
+	@Test
+	void cseTest() {
+		int result=c.add(2, 3);
+		assertEquals(5,result);
+	}
+	@Test
+	void itTest() {
+		int result=c.mul(2, 3);
+	 assertEquals(6,result);
+	}
+	}
+		
